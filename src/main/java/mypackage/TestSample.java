4a5930ce31a4905633145a06c11d2268e8ba4576
prernacbandi-tech <prernacbandi@gmail.com>
2026-04-29 07:57:36 +0530

    a

--- src/main/java/mypackage/TestSample.java
+++ src/main/java/mypackage/TestSample.java
@@ -4,12 +4,12 @@ public class TestSample {
 
 	void Swapping()
 	{
-		int a=10,b=20,c; // by using third variable
+		int a=10,b=20,c; 
 		c=a;
 		a=b;
 		b=c;
 		System.out.println("a="+a);
-		System.out.println("b="+b);
+		System.out.println("b="+b)
 	}
 
 	public static void main(String[] args) {
